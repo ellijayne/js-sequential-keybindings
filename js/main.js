df1206f05849a955889e1fe3a82aba64ca81e9a7
ellijayne <ellijayne.m.willard@gmail.com>
2019-04-22 11:42:33 +1000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    //below is ES6, this would have been original document.addEventListener('keydown', function (event) {});
+
+   document.addEventListener('keydown', event => {
+        
+   })
+});
