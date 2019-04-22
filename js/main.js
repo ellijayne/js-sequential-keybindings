b2eb17a161b1dbc1289e3e949be5f5c49449a047
ellijayne <ellijayne.m.willard@gmail.com>
2019-04-22 12:10:31 +1000

    wrapped global vars and event listener in function to limit scope

--- js/main.js
+++ js/main.js
@@ -1,39 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    let buffer = [];
-    let lastKeyTime = Date.now();
+    keyChecker();
 
-    //below is ES6, this would have been original document.addEventListener('keydown', function (event) {});
-    // we are providing the event parameter to the event handler callback function. We will need that event parameter to check which key is being pressed.
+    function keyChecker() {
 
-   document.addEventListener('keydown', event => {
-        const key = event.key.toLowerCase();
-        const charList ='abcdefghijklmnopqrstuvwxyz0123456789';
-       
-        // console.log(event); this shows what properties are available to us in event. 
+        let buffer = [];
+        let lastKeyTime = Date.now();
 
-        //only want alphanumeric keys:
-        if (charList.indexOf(key) === -1) return;
+        //below is ES6, this would have been original document.addEventListener('keydown', function (event) {});
+        // we are providing the event parameter to the event handler callback function. We will need that event parameter to check which key is being pressed.
 
-        //Limit the time interval between key presses
+        document.addEventListener('keydown', event => {
+            const key = event.key.toLowerCase();
+            const charList ='abcdefghijklmnopqrstuvwxyz0123456789';
         
-        const currentTime = Date.now();
-        // 1000 miniseconds / 1 second
-        if (currentTime - lastKeyTime > 1000) {
-            buffer = [];
-        }
+            // console.log(event); this shows what properties are available to us in event. 
 
-        //Saving the entered key sequence:
-        buffer.push(key);
-        lastKeyTime = currentTime;
+            //only want alphanumeric keys:
+            if (charList.indexOf(key) === -1) return;
 
-        //the images are named the same as the key sequences, so all we need to do is to read the input, make it a string, and set that as the url for the background image.
+            //Limit the time interval between key presses
+            
+            const currentTime = Date.now();
+            // 1000 miniseconds / 1 second
+            if (currentTime - lastKeyTime > 1000) {
+                buffer = [];
+            }
 
-        const container = document.querySelector('#background');
-        container.style.backgroundImage = `url(images/${buffer.join('')}.jpg)`;
+            //Saving the entered key sequence:
+            buffer.push(key);
+            lastKeyTime = currentTime;
 
+            //the images are named the same as the key sequences, so all we need to do is to read the input, make it a string, and set that as the url for the background image.
 
-     
-   })
+            const container = document.querySelector('#background');
+            container.style.backgroundImage = `url(images/${buffer.join('')}.jpg)`;
+
+        })
+    } //keyChecker()
 });
